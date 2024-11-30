d4a8c8938f2b5e605a007a50cc004f2d092f9988
cushonz <cushonz@cwu.edu>
2024-11-29 20:35:39 -0800

    Hide logos based on state

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,17 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSquareGithub } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { Link, useLocation } from "react-router-dom";
 
 function Header() {
+  const location = useLocation();
+  const hideOnRoutes = ["/contact"];
+  const isHiddenRoute = hideOnRoutes.includes(location.pathname);
+
+  const isContactRoute = location.pathname === "/contact";
+  const isHomeRoute = location.pathname === "/";
+
   return (
     <div className="grid grid-cols-5 w-screen h-20">
       <div className="flex justify-around items-center col-start-5">
@@ -14,9 +22,16 @@ function Header() {
         >
           <FontAwesomeIcon icon={faSquareGithub} size="2x" />
         </a>
-        <Link to="/contact" className="text-gray-600 hover:text-gray-900">
-          <FontAwesomeIcon icon={faEnvelope} size="2x" />
-        </Link>
+        {!isContactRoute && (
+          <Link to="/contact" className="text-gray-600 hover:text-gray-900">
+            <FontAwesomeIcon icon={faEnvelope} size="2x" />
+          </Link>
+        )}
+        {!isHomeRoute && (
+          <Link to="/" className="text-gray-600 hover:text-gray-900">
+            <FontAwesomeIcon icon={faHome} size="2x" />
+          </Link>
+        )}
       </div>
     </div>
   );
