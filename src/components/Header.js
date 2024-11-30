377f421ba4c0aca4faf8bea7e7cfdc09a666094f
cushonz <cushonz@cwu.edu>
2024-11-29 20:58:27 -0800

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,13 +15,10 @@ function Header() {
 
   return (
     <div className="grid grid-cols-5 w-screen h-20">
+      <div className="flex items-center ml-4">
+        <h1 className="text-white text-3xl">Cushon Design</h1>
+      </div>
       <div className="flex justify-around items-center col-start-5">
-        <a
-          href="https://github.com/cushonz"
-          className="text-gray-600 hover:text-gray-900"
-        >
-          <FontAwesomeIcon icon={faSquareGithub} size="2x" />
-        </a>
         {!isContactRoute && (
           <Link to="/contact" className="text-gray-600 hover:text-gray-900">
             <FontAwesomeIcon icon={faEnvelope} size="2x" />
@@ -32,6 +29,12 @@ function Header() {
             <FontAwesomeIcon icon={faHome} size="2x" />
           </Link>
         )}
+        <a
+          href="https://github.com/cushonz"
+          className="text-gray-600 hover:text-gray-900"
+        >
+          <FontAwesomeIcon icon={faSquareGithub} size="2x" />
+        </a>
       </div>
     </div>
   );
