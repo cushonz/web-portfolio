68f5497c39749b09ea48491b1e203846569122fa
cushonz <cushonz@cwu.edu>
2024-11-30 04:51:23 -0800

    Link buttons to contact page, place holder

--- src/components/services.js
+++ src/components/services.js
@@ -2,29 +2,39 @@ import React from "react";
 import Plan1 from "./plan1";
 import Plan2 from "./plan2";
 import Plan3 from "./plan3";
+import { Link } from "react-router-dom";
 
 function Services() {
   return (
     <div className="flex flex-col md:grid md:grid-cols-3 my-20 gap-5 sm:gap-10 text-white">
       <div className="flex flex-col bg-gray-950 bg-opacity-50 lg:rounded-md p-20 sm:p-1 sm:gap-y-10">
         <Plan1 />
-        <button className="mt-auto px-4 py-2 bg-blue-500 text-white rounded-md">
-          Learn more
-        </button>
+        <Link
+          className="mt-auto px-4 py-2 bg-[#A78BFA] text-[#4C1D95] rounded-md dark:bg-[#5b21b6] dark:hover:bg-[#7C3AED] dark:text-[#EDEDED] text-center"
+          to="/contact"
+        >
+          Learn More
+        </Link>
       </div>
 
       <div className="flex flex-col bg-gray-950 bg-opacity-50 lg:rounded-md p-20 sm:p-1 sm:gap-y-10">
         <Plan2 />
-        <button className="mt-auto px-4 py-2 bg-blue-500 text-white rounded-md">
-          Learn more
-        </button>
+        <Link
+          className="mt-auto px-4 py-2 bg-[#A78BFA] text-[#4C1D95] rounded-md dark:bg-[#5b21b6] dark:hover:bg-[#7C3AED] dark:text-[#EDEDED] text-center"
+          to="/contact"
+        >
+          Learn More
+        </Link>
       </div>
 
       <div className="flex flex-col bg-gray-950 bg-opacity-50 lg:rounded-md p-20 sm:p-1 sm:gap-y-10">
         <Plan3 />
-        <button className="mt-auto px-4 py-2 bg-blue-500 text-white rounded-md">
-          Learn more
-        </button>
+        <Link
+          className="mt-auto px-4 py-2 bg-[#A78BFA] text-[#4C1D95] rounded-md dark:bg-[#5b21b6] dark:hover:bg-[#7C3AED] dark:text-[#EDEDED] text-center"
+          to="/contact"
+        >
+          Learn More
+        </Link>
       </div>
     </div>
   );
