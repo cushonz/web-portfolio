bcb940e07227879c868f92dc9db7c02b65a4e4e6
cushonz <cushonz@cwu.edu>
2024-11-29 23:33:02 -0800

    Installed sqlite3 and express

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
+// Home.js
 import React from "react";
-import "./styles/App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-import Skills from "./components/skills";
-import Services from "./components/services";
+import Home from "./components/home";
+import Contact from "./components/contact";
+import "./styles/App.css";
 import "./index.css"; // Adjust if you named the file differently
 
-import intro from "./intro.json";
-
 function App() {
-  const profilePic = "https://avatars.githubusercontent.com/u/77802000?v=4";
   return (
-    <div>
-      <Header />
-      <div className="flex flex-wrap flex-col intro text-center p-4 sm:items-center sm:justify-between sm:p-8 w-screen box-border">
-        <p className="text-lg sm:text-xl lg:text-2xl">{intro.introText}</p>
-        <img
-          src={profilePic}
-          alt="Profile"
-          className="mt-4 sm:mt-0 sm:w-32 sm:h-32 md:w-40 md:h-40 lg:w-72 lg:h-72 rounded-full mx-auto sm:mx-0"
-        />
-      </div>
-
-      <Skills />
-      <Services />
-    </div>
+    <Router>
+      {/* Routes to be displayed here */}
+      <Routes>
+        <Route path="/" element={<Home />} /> {/* Placeholder route */}
+        <Route path="/contact" element={<Contact />} />{" "}
+        {/* Placeholder route */}
+      </Routes>
+    </Router>
   );
 }
 
